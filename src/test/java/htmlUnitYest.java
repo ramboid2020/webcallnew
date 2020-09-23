acba09eb3e7842f226e7c05a7eebef542f3ade2f
root <root@localhost.localdomain>
2020-09-22 21:55:22 -0400

    Updated code

--- src/test/java/htmlUnitYest.java
+++ src/test/java/htmlUnitYest.java
@@ -1,16 +1,15 @@
 package ac;
 
-import org.openqa.selenium.By;		
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;	
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-
 import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
 import org.junit.Assert;
 
 import org.slf4j.Logger;
@@ -18,28 +17,23 @@ import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-import java.io.*;
-import java.util.*;
-		
 public class htmlUnitYest {				
-	// Reference to file anme with urls and ports
-	//private static String configFile = "src/test/resources/urls/config.txt";
+	private static Logger log = LoggerFactory.getLogger(htmlUnitYest.class);
 
-	Logger log = LoggerFactory.getLogger(htmlUnitYest.class);
+	private static WebDriver driver = null;
+
+	@BeforeClass
+	public static void htmlUnitYestTestBeforeClass() throws IOException {
+		// Create driver
+		driver = new HtmlUnitDriver();
+	}
  
 	@Test
 	public void htmlUnitYestTest() throws IOException {
 		log.info("Start test of web page: {}", "Welcome to Maven World ....");
 
-		// Class references
-		WebDriver driver = null;
-
 		// Main try-catch statement
 		try {
-			// Create driver instance
-                	driver = new HtmlUnitDriver();
-                    
-
 			// Retrieve website parameters
 			String url = System.getProperty("app.domain");
 			
@@ -72,7 +66,6 @@ public class htmlUnitYest {
 				} else {
 					url = "http://" + url.trim();
 					System.out.println("DOMAIN: " + url.trim());
-
 				}
 				strURL = Util.getHost(url.trim());
 				strProtocol = Util.getProtocol(url.trim());
@@ -85,6 +78,7 @@ public class htmlUnitYest {
 				// Check page title
 				if ( driver.getTitle().trim() != "") {
 					log.info("Page title is: {}", driver.getTitle());
+					try { Assert.assertTrue("Websites are valid!", true); } catch(Exception oaf){};
 				} else {
 					log.info("Page title is: {}", "Empty");
 					try { Assert.fail("Invalid website title: Null value"); } catch(Exception oaf){}
@@ -95,12 +89,12 @@ public class htmlUnitYest {
 			}
 		} catch(Exception eek) {
 			try { Assert.fail("Failed to open website"); } catch(Exception oaf){}
-		} finally {
-			// Close file reader
-			//try { reader.close(); } catch(Exception oaf) {}
-
-			log.info("Quitting driver");
-			try { driver.quit(); } catch(Exception oaf){}
 		}			
-	}		
+	}
+
+	@AfterClass
+	public static void logout() {
+		log.info("Quiting driver");
+		try { driver.quit(); } catch(Exception oaf){}
+	}
 }
